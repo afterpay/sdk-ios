1445e235eb10c7063268bd0ec039abd51b7fea8d
Adam Campbell <adam.campbell@ittybittyapps.com>
2020-06-04 11:20:25 +1000

    Add testing target for Afterpay SDK framework

---
+++ AfterpayTests/AfterpayTests.swift
@@ -0,0 +1,16 @@
+//
+//  AfterpayTests.swift
+//  AfterpayTests
+//
+//  Created by Adam Campbell on 4/6/20.
+//  Copyright © 2020 Afterpay. All rights reserved.
+//
+
+import XCTest
+
+class AfterpayTests: XCTestCase {
+
+    func testExample() throws {
+    }
+
+}
