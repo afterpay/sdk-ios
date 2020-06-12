d6ad5ab0465c927c24334e1bac80d7c42ab41de9
Adam Campbell <adam.campbell@ittybittyapps.com>
2020-06-12 17:57:26 +1000

    Handle keyboard events in DataEntryViewController

--- Example/Example/DataEntryViewController.swift
+++ Example/Example/DataEntryViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-final class DataEntryViewController: UIViewController {
+final class DataEntryViewController: UIViewController, UITextFieldDelegate {
+
+  private var centeringConstraint: NSLayoutConstraint!
 
   override func loadView() {
     view = UIView()
@@ -17,6 +19,8 @@ final class DataEntryViewController: UIViewController {
     let emailTextField = UITextField()
     emailTextField.placeholder = "example@test.com"
     emailTextField.borderStyle = .roundedRect
+    emailTextField.returnKeyType = .done
+    emailTextField.delegate = self
 
     let submitButton = UIButton(type: .system)
     submitButton.setTitle("Checkout with Afterpay", for: .normal)
@@ -30,12 +34,62 @@ final class DataEntryViewController: UIViewController {
 
     let layoutGuide = view.readableContentGuide
 
+    centeringConstraint = stackView.centerYAnchor.constraint(equalTo: layoutGuide.centerYAnchor)
+
     NSLayoutConstraint.activate([
+      centeringConstraint,
       stackView.leadingAnchor.constraint(equalTo: layoutGuide.leadingAnchor),
       stackView.topAnchor.constraint(greaterThanOrEqualTo: layoutGuide.topAnchor),
       stackView.trailingAnchor.constraint(equalTo: layoutGuide.trailingAnchor),
-      stackView.centerYAnchor.constraint(equalTo: layoutGuide.centerYAnchor),
     ])
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    let notificationCenter = NotificationCenter.default
+
+    notificationCenter.addObserver(
+      self,
+      selector: #selector(adjustForKeyboard),
+      name: UIResponder.keyboardWillHideNotification,
+      object: nil
+    )
+
+    notificationCenter.addObserver(
+      self,
+      selector: #selector(adjustForKeyboard),
+      name: UIResponder.keyboardWillChangeFrameNotification,
+      object: nil
+    )
+  }
+
+  // MARK: UITextFieldDelegate
+
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    textField.resignFirstResponder()
+    return true
+  }
+
+  // MARK: Keyboard Handling
+
+  @objc private func adjustForKeyboard(notification: Notification) {
+    let endFrameKey = UIResponder.keyboardFrameEndUserInfoKey
+    guard let keyboardValue = notification.userInfo?[endFrameKey] as? NSValue else { return }
+
+    let keyboardScreenEndFrame = keyboardValue.cgRectValue
+    let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+
+    if notification.name == UIResponder.keyboardWillHideNotification {
+      centeringConstraint.constant = 0
+    } else {
+      let layoutGuide = view.readableContentGuide
+      centeringConstraint.constant = keyboardViewEndFrame.origin.y - layoutGuide.layoutFrame.midY
+    }
+
+    UIView.animate(withDuration: 0.5) {
+      self.view.layoutIfNeeded()
+    }
   }
 
 }
