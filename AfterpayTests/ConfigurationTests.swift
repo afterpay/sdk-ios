56d3a61445081e4076e7ed091d384e2f6edb9e89
Adam Campbell <adam.campbell@ittybittyapps.com>
2020-07-24 15:49:56 +1000

    Add no minimum and invalid currency code checks

--- AfterpayTests/ConfigurationTests.swift
+++ AfterpayTests/ConfigurationTests.swift
@@ -11,11 +11,31 @@ import XCTest
 
 class ConfigurationTests: XCTestCase {
 
+  let one = "1.00"
+  let oneThousand = "1000.00"
+
+  let usdCode = "USD"
+  let invalidCode = "XXX"
+
   func testValidConfiguration() throws {
-    _ = try Configuration(
-      minimumAmount: "1.00",
-      maximumAmount: "10.00",
-      currencyCode: "USD")
+    _ = try Configuration(minimumAmount: one, maximumAmount: oneThousand, currencyCode: usdCode)
+  }
+
+  func testValidConfigurationNoMinimum() throws {
+    _ = try Configuration(minimumAmount: nil, maximumAmount: oneThousand, currencyCode: usdCode)
+  }
+
+  func testInvalidConfigurationInvalidCurrencyCode() {
+    do {
+      _ = try Configuration(
+        minimumAmount: one,
+        maximumAmount: oneThousand,
+        currencyCode: invalidCode)
+    } catch ConfigurationError.invalidCurrencyCode(invalidCode) {
+    } catch {
+      let expectedError = ConfigurationError.invalidCurrencyCode(invalidCode)
+      XCTFail("Failed to match expected error \(expectedError)")
+    }
   }
 
 }
