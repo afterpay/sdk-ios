f372b85973f78c4355a9fb0868580014fedfd913
Adam Campbell <adam.campbell@ittybittyapps.com>
2020-07-28 14:16:41 +1000

    Add test target to SPM

--- Package.swift
+++ Package.swift
@@ -14,5 +14,6 @@ let package = Package(
   dependencies: [],
   targets: [
     .target(name: "Afterpay", dependencies: []),
+    .testTarget(name: "AfterpayTests", dependencies: ["Afterpay"], path: "AfterpayTests"),
   ]
 )
