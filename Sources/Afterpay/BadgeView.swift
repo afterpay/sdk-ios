39d939e74660fab2747dbfb188bdf3f3e302346a
Adam Campbell <adam.campbell@ittybittyapps.com>
2020-07-31 14:22:51 +1000

    Add preliminary version of the badge view

---
+++ Sources/Afterpay/BadgeView.swift
@@ -0,0 +1,41 @@
+//
+//  BadgeView.swift
+//  Afterpay
+//
+//  Created by Adam Campbell on 31/7/20.
+//  Copyright © 2020 Afterpay. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public final class BadgeView: UIView {
+
+  private var svgView: SVGView!
+
+  init() {
+    super.init(frame: .zero)
+
+    sharedInit()
+  }
+
+  required init?(coder: NSCoder) {
+    super.init(coder: coder)
+
+    sharedInit()
+  }
+
+  func sharedInit() {
+    // TODO: Select style
+    svgView = SVGView(svg: .badgeWhiteOnBlack)
+    svgView.translatesAutoresizingMaskIntoConstraints = false
+
+    NSLayoutConstraint.activate([
+      svgView.leadingAnchor.constraint(equalTo: leadingAnchor),
+      svgView.trailingAnchor.constraint(equalTo: trailingAnchor),
+      svgView.topAnchor.constraint(equalTo: topAnchor),
+      svgView.bottomAnchor.constraint(equalTo: bottomAnchor),
+    ])
+  }
+
+}
