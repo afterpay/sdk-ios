3534c69055b715f5d2351e933118b32b5cc30f70
Adam Campbell <adam.campbell@ittybittyapps.com>
2020-07-31 16:14:09 +1000

    Set up ComponentViewController with light content and dark content

--- Example/Example/Components/ComponentsViewController.swift
+++ Example/Example/Components/ComponentsViewController.swift
@@ -29,11 +29,13 @@ final class ComponentsViewController: UIViewController {
     scrollView.addSubview(contentStack)
 
     let stackConstraints = [
-      contentStack.leadingAnchor.constraint(equalTo: layoutGuide.leadingAnchor),
-      contentStack.trailingAnchor.constraint(equalTo: layoutGuide.trailingAnchor),
+      contentStack.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+      contentStack.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
       contentStack.topAnchor.constraint(equalTo: layoutGuide.topAnchor),
-      contentStack.widthAnchor.constraint(equalTo: layoutGuide.widthAnchor),
+      contentStack.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
     ]
+
+    // Light Content
 
     let lightContentView = UIView()
     lightContentView.backgroundColor = .white
@@ -48,23 +50,61 @@ final class ComponentsViewController: UIViewController {
     lightContentView.addSubview(lightContentStack)
 
     let lightContentStackConstraints = [
-      lightContentStack.leadingAnchor.constraint(equalTo: lightContentView.leadingAnchor),
-      lightContentStack.trailingAnchor.constraint(equalTo: lightContentView.trailingAnchor),
-      lightContentStack.topAnchor.constraint(equalTo: lightContentView.topAnchor),
-      lightContentStack.bottomAnchor.constraint(equalTo: lightContentView.bottomAnchor),
+      lightContentStack.leadingAnchor.constraint(equalTo: lightContentView.readableContentGuide.leadingAnchor),
+      lightContentStack.trailingAnchor.constraint(equalTo: lightContentView.readableContentGuide.trailingAnchor),
+      lightContentStack.topAnchor.constraint(equalTo: lightContentView.topAnchor, constant: 8),
+      lightContentStack.bottomAnchor.constraint(equalTo: lightContentView.bottomAnchor, constant: -8),
     ]
 
     let lightContentLabel = UILabel()
     lightContentLabel.text = "Light components:"
     lightContentStack.addArrangedSubview(lightContentLabel)
 
-    let badge = BadgeView(style: .whiteOnBlack)
-    let badgeConstraints = [badge.widthAnchor.constraint(equalToConstant: 64)]
+    let lightBadge = BadgeView(style: .whiteOnBlack)
+    let lightBadgeConstraints = [lightBadge.widthAnchor.constraint(equalToConstant: 64)]
 
-    let badgeStack = UIStackView(arrangedSubviews: [badge, UIView()])
-    lightContentStack.addArrangedSubview(badgeStack)
+    let lightBadgeStack = UIStackView(arrangedSubviews: [lightBadge, UIView()])
+    lightContentStack.addArrangedSubview(lightBadgeStack)
 
-    NSLayoutConstraint.activate(stackConstraints + lightContentStackConstraints + badgeConstraints)
+    // Dark Content
+
+    let darkContentView = UIView()
+    darkContentView.backgroundColor = .black
+
+    contentStack.addArrangedSubview(darkContentView)
+
+    let darkContentStack = UIStackView()
+    darkContentStack.translatesAutoresizingMaskIntoConstraints = false
+    darkContentStack.axis = .vertical
+    darkContentStack.spacing = 8
+
+    darkContentView.addSubview(darkContentStack)
+
+    let darkContentStackConstraints = [
+      darkContentStack.leadingAnchor.constraint(equalTo: darkContentView.readableContentGuide.leadingAnchor),
+      darkContentStack.trailingAnchor.constraint(equalTo: darkContentView.readableContentGuide.trailingAnchor),
+      darkContentStack.topAnchor.constraint(equalTo: darkContentView.topAnchor, constant: 8),
+      darkContentStack.bottomAnchor.constraint(equalTo: darkContentView.bottomAnchor, constant: -8),
+    ]
+
+    let darkContentLabel = UILabel()
+    darkContentLabel.textColor = .white
+    darkContentLabel.text = "Dark components:"
+    darkContentStack.addArrangedSubview(darkContentLabel)
+
+    let darkBadge = BadgeView(style: .blackOnWhite)
+    let darkBadgeConstraints = [darkBadge.widthAnchor.constraint(equalToConstant: 64)]
+
+    let darkBadgeStack = UIStackView(arrangedSubviews: [darkBadge, UIView()])
+    darkContentStack.addArrangedSubview(darkBadgeStack)
+
+    NSLayoutConstraint.activate(
+      stackConstraints +
+      lightContentStackConstraints +
+      lightBadgeConstraints +
+      darkContentStackConstraints +
+      darkBadgeConstraints
+    )
 
     self.view = scrollView
   }
