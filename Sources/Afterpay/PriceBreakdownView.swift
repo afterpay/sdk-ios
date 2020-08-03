8f08ecdb90074ba2a14f50d173d868667fee317f
Adam Campbell <adam.campbell@ittybittyapps.com>
2020-08-03 11:54:28 +1000

    Add shared initializer to PriceBreakdownView

--- Sources/Afterpay/PriceBreakdownView.swift
+++ Sources/Afterpay/PriceBreakdownView.swift
@@ -15,10 +15,35 @@ public final class PriceBreakdownView: UIView {
 
   public init() {
     super.init(frame: .zero)
+
+    sharedInit()
   }
 
   required init?(coder: NSCoder) {
     super.init(coder: coder)
+
+    sharedInit()
+  }
+
+  private func sharedInit() {
+    let attributedString = NSMutableAttributedString(string: "or 4 payments of $40.00 with")
+
+    label.attributedText = attributedString
+
+    if #available(iOS 13.0, *) {
+      label.textColor = .label
+    } else {
+      label.textColor = .black
+    }
+
+    addSubview(label)
+
+    NSLayoutConstraint.activate([
+      label.leadingAnchor.constraint(equalTo: leadingAnchor),
+      label.topAnchor.constraint(equalTo: topAnchor),
+      label.trailingAnchor.constraint(equalTo: trailingAnchor),
+      label.bottomAnchor.constraint(equalTo: bottomAnchor),
+    ])
   }
 
 }
