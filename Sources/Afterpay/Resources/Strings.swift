7329005ea856fecd2385ee4f594384e0f594e85e
Adam Campbell <adam.campbell@ittybittyapps.com>
2020-08-12 17:15:26 +1000

    Use strings defined in one place for the price breakdown string

---
+++ Sources/Afterpay/Resources/Strings.swift
@@ -0,0 +1,25 @@
+//
+//  Strings.swift
+//  Afterpay
+//
+//  Created by Adam Campbell on 12/8/20.
+//  Copyright © 2020 Afterpay. All rights reserved.
+//
+
+import Foundation
+
+// These should be transitioned to a `.strings` file once resource support is available for
+// swift packages
+enum Strings {
+
+  // MARK: - Static Strings
+
+  static let orPayWith = "or pay with"
+
+  // MARK: - String Formats
+
+  static let availableBetweenFormat = "available for orders between %@ - %@"
+  static let availableUpToFormat = "available for orders up to %@"
+  static let fourPaymentsFormat = "or 4 interest-free payments of %@ with"
+
+}
