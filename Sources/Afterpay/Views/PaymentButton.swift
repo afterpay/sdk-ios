0ffe4ef558ec4c5da7ac28cf6d207407cdaad737
Adam Campbell <adam.campbell@ittybittyapps.com>
2020-11-30 11:31:15 +1100

    Add PaymentButton

---
+++ Sources/Afterpay/Views/PaymentButton.swift
@@ -0,0 +1,13 @@
+//
+//  PaymentButton.swift
+//  Afterpay
+//
+//  Created by Adam Campbell on 27/11/20.
+//  Copyright © 2020 Afterpay. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public final class PaymentButton: UIButton {
+}
