b844b9c923dbfd0b5f87b0162931f70be7e612ca
Adam Campbell <adam.campbell@ittybittyapps.com>
2020-11-30 16:15:34 +1100

    Implement Payment Button and use it in the example project

--- Sources/Afterpay/Views/PaymentButton.swift
+++ Sources/Afterpay/Views/PaymentButton.swift
@@ -10,4 +10,64 @@ import Foundation
 import UIKit
 
 public final class PaymentButton: UIButton {
+
+  public var colorScheme: ColorScheme = .static(.blackOnMint) {
+    didSet { renderImage() }
+  }
+
+  private var configuration: SVGConfiguration {
+    PaymentButtonConfiguration(colorScheme: colorScheme)
+  }
+
+  public init(colorScheme: ColorScheme = .static(.blackOnMint)) {
+    self.colorScheme = colorScheme
+
+    super.init(frame: .zero)
+
+    sharedInit()
+  }
+
+  required init?(coder: NSCoder) {
+    super.init(coder: coder)
+
+    sharedInit()
+  }
+
+  private func sharedInit() {
+    let locale = getConfiguration()?.locale ?? Locales.unitedStates
+    let svg = configuration.svg(localizedFor: locale, withTraits: traitCollection)
+
+    NSLayoutConstraint.activate([
+      heightAnchor.constraint(equalTo: widthAnchor, multiplier: svg.aspectRatio),
+      widthAnchor.constraint(greaterThanOrEqualToConstant: svg.minimumWidth),
+    ])
+
+    translatesAutoresizingMaskIntoConstraints = false
+    adjustsImageWhenHighlighted = true
+    adjustsImageWhenDisabled = true
+  }
+
+  private var previousBounds: CGRect = .zero
+
+  public override func layoutSubviews() {
+    super.layoutSubviews()
+
+    if bounds != previousBounds {
+      renderImage()
+      previousBounds = bounds
+    }
+  }
+
+  private func renderImage() {
+    let svgView = SVGView(svgConfiguration: configuration)
+    svgView.frame = bounds
+
+    let renderer = UIGraphicsImageRenderer(size: svgView.bounds.size)
+    let image = renderer.image { rendererContext in
+      svgView.layer.render(in: rendererContext.cgContext)
+    }
+
+    setImage(image, for: .normal)
+  }
+
 }
