f8b3e4358b9d4a831576116456365b3a0cbbffea
Adam Campbell <adam.campbell@ittybittyapps.com>
2020-12-04 17:08:06 +1100

    Add ExpressCheckoutMessage for decoding express JSON messages

---
+++ Sources/Afterpay/Checkout/ExpressCheckoutMessage.swift
@@ -0,0 +1,55 @@
+//
+//  ExpressCheckoutMessage.swift
+//  Afterpay
+//
+//  Created by Adam Campbell on 4/12/20.
+//  Copyright © 2020 Afterpay. All rights reserved.
+//
+
+import Foundation
+
+struct ExpressCheckoutMessage: Decodable {
+  let requestId: String
+  let event: Event?
+
+  enum Event {
+    case shippingAddressDidChange(Address)
+  }
+
+  struct Address: Decodable {
+    var countryCode: String?
+    var postcode: String?
+    var phoneNumber: String?
+    var state: String?
+    var suburb: String?
+  }
+
+  private enum CodingKeys: String, CodingKey {
+    case type, meta, payload
+  }
+
+  private enum MetaCodingKeys: String, CodingKey {
+    case requestId
+  }
+
+  private enum MessageType: String, Decodable {
+    case onShippingAddressChange
+  }
+
+  init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    let metaContainer = try container.nestedContainer(keyedBy: MetaCodingKeys.self, forKey: .meta)
+
+    requestId = try metaContainer.decode(String.self, forKey: .requestId)
+
+    let type = try? container.decode(MessageType.self, forKey: .type)
+
+    switch type {
+    case .onShippingAddressChange:
+      let address = try container.decode(Address.self, forKey: .payload)
+      event = .shippingAddressDidChange(address)
+    default:
+      event = nil
+    }
+  }
+}
