f66536de00aa636bfdf9cd7408c98e5f0d1703d6
nabby26 <greenybella@yahoo.com>
2021-02-16 13:56:23 +1100

    Add headline labels on welcome page

--- Sources/Afterpay/Consumer Cards/View/WelcomeView.swift
+++ Sources/Afterpay/Consumer Cards/View/WelcomeView.swift
@@ -11,6 +11,27 @@ import UIKit
 
 final class WelcomeView: UIView {
 
+  var verticalStackView: UIStackView = {
+    let stackView = UIStackView()
+    stackView.axis = .vertical
+    stackView.translatesAutoresizingMaskIntoConstraints = false
+    stackView.spacing = 8
+    return stackView
+  }()
+
+  let headlineLabel: (_ text: String) -> UILabel = { text in
+    let label = UILabel()
+    label.text = text
+    label.textAlignment = .left
+
+    let font = UIFont.systemFont(ofSize: 14, weight: .semibold)
+    label.font = UIFontMetrics(forTextStyle: .headline).scaledFont(for: font)
+
+    label.adjustsFontForContentSizeCategory = true
+    label.translatesAutoresizingMaskIntoConstraints = false
+    return label
+  }
+
   init(continueAction: Selector) {
     super.init(frame: .zero)
 
@@ -19,17 +40,30 @@ final class WelcomeView: UIView {
 
     let titleLabel = TitleLabel(with: "Lorem Ipsum\nLorem Ipsum")
 
+    let firstHeadline = headlineLabel("first headline")
+    let secondHeadline = headlineLabel("second headline")
+    let thirdHeadline = headlineLabel("third headline")
+
+    verticalStackView.addArrangedSubview(firstHeadline)
+    verticalStackView.addArrangedSubview(secondHeadline)
+    verticalStackView.addArrangedSubview(thirdHeadline)
+
     addSubview(continueButton)
     addSubview(titleLabel)
+    addSubview(verticalStackView)
 
     NSLayoutConstraint.activate([
       titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 32),
       titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
       titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
 
+      verticalStackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+      verticalStackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+      verticalStackView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 32),
+
       continueButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
       continueButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
-      continueButton.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 24),
+      continueButton.topAnchor.constraint(equalTo: verticalStackView.bottomAnchor, constant: 24),
       continueButton.heightAnchor.constraint(greaterThanOrEqualToConstant: 64),
     ])
   }
