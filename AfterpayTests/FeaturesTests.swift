1f82f4142b3562db8bac93d32e5ef7b0015ce3e4
Huw Rowlands <huw@ittybittyapps.com>
2021-03-11 10:46:28 +1100

    Add features struct, with widget enabled feature
    Previously, there was no way to have feature flags in the SDK. Now, we are using the defaults parsed from the app’s arguments on launch.
    
    With these arguments are passed on app launch: `-com.afterpay.widget-enabled YES`, the SDK will have the `widgetEnabled` feature toggle turned on. If it’s not passed, it is off by default.
    
    Currently, no behaviour is based on this feature flag. Soon, it will be used to turn on the checkout widget.

---
+++ AfterpayTests/FeaturesTests.swift
@@ -0,0 +1,22 @@
+//
+//  FeaturesTests.swift
+//  AfterpayTests
+//
+//  Created by Huw Rowlands on 11/3/21.
+//  Copyright © 2021 Afterpay. All rights reserved.
+//
+
+@testable import Afterpay
+import XCTest
+
+class FeaturesTests: XCTestCase {
+
+  func testWidgetEnabledIsOffByDefault() {
+    XCTAssertFalse(Features.widgetEnabled)
+
+    UserDefaults.standard.setVolatileDomain(["com.afterpay.widget-enabled": true], forName: UserDefaults.argumentDomain)
+
+    XCTAssertTrue(Features.widgetEnabled)
+  }
+
+}
