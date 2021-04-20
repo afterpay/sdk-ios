e8c15d8c070aba54349341dd88705ddcfd5c6066
Huw Rowlands <huw@ittybittyapps.com>
2021-04-21 09:51:09 +1000

    Remove feature toggle for Widget
    Also, add documentation around the SDK’s feature flag support

--- Sources/Afterpay/Features.swift
+++ Sources/Afterpay/Features.swift
@@ -8,13 +8,23 @@
 
 import Foundation
 
+/// Feature flags for the Afterpay SDK.
+///
+/// Afterpay SDK feature flags are driven by UserDefaults and launch arguments. When creating a feature flag, we create
+/// a static property in here to read it. For a given key, the property reads the flag's status from UserDefaults:
+///
+/// ```
+/// UserDefaults.standard.bool(forKey: "com.afterpay.example-key")
+/// ```
+///
+/// Users of the SDK can specify the launch argument: `-com.afterpay.example-key YES` to turn on this feature flag (or
+/// `-com.afterpay.example-key NO` to turn it off).
+///
+/// If this struct has no static properties, it means there are no features currently being flagged.
+///
+/// # See Also
+/// * [SwiftLee](https://www.avanderlee.com/xcode/overriding-userdefaults-launch-arguments/) -
+///   Overriding UserDefaults for improved productivity
 public struct AfterpayFeatures {
-
-  /// Is the checkout widget enabled?
-  ///
-  /// Enable by setting '`-com.afterpay.widget-enabled YES`' argument on launch.
-  public static var widgetEnabled: Bool {
-    UserDefaults.standard.bool(forKey: "com.afterpay.widget-enabled")
-  }
 
 }
