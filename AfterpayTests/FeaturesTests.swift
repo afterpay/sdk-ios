44743cc55db7a4292ef355694f6003bbc70f4b8d
Huw Rowlands <huw@ittybittyapps.com>
2021-04-21 09:51:09 +1000

    Remove feature toggle for Widget
    Also, add documentation around the SDK’s feature flag support

--- AfterpayTests/FeaturesTests.swift
+++ AfterpayTests/FeaturesTests.swift
@@ -12,11 +12,23 @@ import XCTest
 class FeaturesTests: XCTestCase {
 
   func testWidgetEnabledIsOffByDefault() {
-    XCTAssertFalse(AfterpayFeatures.widgetEnabled)
+    XCTAssertFalse(AfterpayFeatures.testFeature)
 
-    UserDefaults.standard.setVolatileDomain(["com.afterpay.widget-enabled": true], forName: UserDefaults.argumentDomain)
+    UserDefaults.standard.setVolatileDomain(["com.afterpay.test-feature": true], forName: UserDefaults.argumentDomain)
 
-    XCTAssertTrue(AfterpayFeatures.widgetEnabled)
+    XCTAssertTrue(AfterpayFeatures.testFeature)
+  }
+
+}
+
+private extension AfterpayFeatures {
+
+  /// A test feature
+  ///
+  /// This doesn't exist in real life, but it is here as an example of how we can read launch arguments and use them as
+  /// flags. Usually, these will be defined in `AfterpayFeatures.swift`.
+  static var testFeature: Bool {
+    UserDefaults.standard.bool(forKey: "com.afterpay.test-feature")
   }
 
 }
