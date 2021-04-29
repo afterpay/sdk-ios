8db9bceea1f4697d5d51f0235f8ec6d1f28b4eed
Huw Rowlands <huw@ittybittyapps.com>
2021-04-30 09:57:06 +1000

    Add UI test target to Example app

---
+++ Example/ExampleUITests/ExampleUITests.swift
@@ -0,0 +1,18 @@
+//
+//  ExampleUITests.swift
+//  ExampleUITests
+//
+//  Created by Huw Rowlands on 30/4/21.
+//  Copyright © 2021 Afterpay. All rights reserved.
+//
+
+import XCTest
+
+class ExampleUITests: XCTestCase {
+
+  func testExampleAppLaunches() throws {
+    let app = XCUIApplication()
+    app.launch()
+  }
+
+}
