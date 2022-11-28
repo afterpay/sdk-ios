bbbbb4c89e3b39142455a1aaf905660723bf040c
Scott Antonac <santonac@squareup.com>
2022-11-28 16:51:06 +1100

    EIT-1889: add compact badge view and option for breakdown

---
+++ Sources/Afterpay/Views/CompactBadgeView.swift
@@ -0,0 +1,33 @@
+//
+//  BadgeView.swift
+//  Afterpay
+//
+//  Created by Adam Campbell on 31/7/20.
+//  Copyright © 2020 Afterpay. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class CompactBadgeView: AfterpayLogo {
+  override public init(colorScheme: ColorScheme = .static(.blackOnMint)) {
+    super.init(colorScheme: colorScheme)
+  }
+
+  required init?(coder: NSCoder) {
+    super.init(coder: coder)
+  }
+
+  override internal func getImageName(brand: String, color: String) -> String {
+    return "compact-badge-\(brand)-\(color)"
+  }
+
+  override internal func getImageColor() -> String {
+    var color = colorScheme.lightPalette.slug
+    if traitCollection.userInterfaceStyle == .dark {
+      color = colorScheme.darkPalette.slug
+    }
+
+    return color
+  }
+}
